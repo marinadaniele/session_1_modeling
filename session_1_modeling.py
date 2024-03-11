74c40c57bd14bdf5bb6b57330255abf3675f35fb
marinadaniele <marinadanielee@gmail.com>
2024-03-11 16:21:08 +0100

    commit 1

---
+++ session_1_modeling.py
@@ -0,0 +1,21 @@
+# object-oriented programming (OOP)
+# data science, regressions, time series, applicable coding.
+# OOP: different way of looking at programming.
+# Programming paradigm: a philosophy or style of programming.
+# We map real life situations into programming language.
+# we use objects to model our problem and solve it.
+# We structure programs in a way such as properties bundled together in an object.
+# car: wheels, engines, doors, windows...
+# we model these objects in as objects which have data and methods (functions) they can perform.
+
+# class: a blueprint for creating objects.
+# Everything in python is an object
+
+    # each object has:
+        # a type
+        # an internal data representation
+        # a set of procedures for interacting with the object.
+
+# def defines a function, class defines and object / class
+# capitalize the first letter of the class name for convention to distinguish it from a function.
+class Point:
